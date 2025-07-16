a913d945d0948ced668488d9a822d2a0fd1fc4e9
unknown <Med Sidatt>
2025-07-16 10:12:11 +0000

    Addin requirements.txt file

--- demo/whatsappbot/urls.py
+++ demo/whatsappbot/urls.py
@@ -4,7 +4,7 @@ from .views import whatsapp_webhook, message_status_callback, whatsapp_message_r
 urlpatterns = [
     path('webhook/', whatsapp_webhook, name='whatsapp_webhook'),
     path('hugging/', hugging_face_test, name='hugging_face_test'),
-    path('message/receive', whatsapp_message_receive, name='whatsapp_message_receive'),
+    # path('message/receive', whatsapp_message_receive, name='whatsapp_message_receive'),
     path('status/', message_status_callback, name='message_status')
 
 ]
